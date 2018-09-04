5f205dc6b0e9a211d7e70aca82ba0317c53df704
daniel sim <daniel@LAPTOP-VIQA6AQL.localdomain>
2018-09-04 16:37:38 +0800

    fix test case

--- test/subzones.js
+++ test/subzones.js
@@ -2,13 +2,13 @@ const index = require('../index')
 
 describe("Simple tests on subzones", () => {
   it("Should match a subzone correctly", async () => {
-    expect(index.getSubzoneAtPoint([103.893187, 1.302925]).properties.nice_name)
+    expect(index.getSubzoneAtPoint([103.893187, 1.302925]).properties.niceName)
       .toBe("Mountbatten")
 
-    expect(index.getSubzoneAtPoint([103.673784, 1.256942]).properties.nice_name)
+    expect(index.getSubzoneAtPoint([103.673784, 1.256942]).properties.niceName)
       .toBe("Jurong Island And Bukom")
 
-    expect(index.getSubzoneAtPoint([103.777475, 1.295793]).properties.nice_name)
+    expect(index.getSubzoneAtPoint([103.777475, 1.295793]).properties.niceName)
       .toBe("National University Of Singapore")
   })
 })
